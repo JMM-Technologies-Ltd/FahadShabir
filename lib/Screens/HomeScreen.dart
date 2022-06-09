bc65a98b8076bbbcaf23fae27884b8ab7922a82b
jojkhan <joj.khan.5815@gmail.com>
2022-06-09 15:32:05 +0500

    fahad

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,3 +1,5 @@
+import 'package:fahadshabir/Colors/Config.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,6 +10,46 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Good morning Akila !"),
+        elevation: 0,
+        actions: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: IconButton(
+              splashRadius: 20.0,
+              onPressed: (){},
+              icon: Icon(CupertinoIcons.cart_fill),
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar:BottomNavigationBar(
+        selectedItemColor:Config.mainColor,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        unselectedLabelStyle: TextStyle(color: Colors.grey),
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag),
+              label: "Offers"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.window),
+              label: "Menu"
+          ),
+
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "profile"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.increase_indent),
+              label: "More"
+          ),
+        ],
       ),
     );
   }
