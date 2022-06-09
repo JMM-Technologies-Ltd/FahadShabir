a030ce657634cc3c30fdc7a4fe26fba61de33aaa
jojkhan <joj.khan.5815@gmail.com>
2022-06-09 13:34:01 +0500

    fahad

--- lib/Colors/Config.dart
+++ lib/Colors/Config.dart
@@ -4,4 +4,5 @@ import 'package:flutter/material.dart';
 class Config
 {
   static Color mainColor=const Color(0xffFC6011);
+  static Color fbColor=const Color(0xff367FC0);
 }
--- lib/Widgets/mIconButton.dart
+++ lib/Widgets/mIconButton.dart
@@ -3,22 +3,24 @@ import 'package:flutter/material.dart';
 import '../Colors/Config.dart';
 
 class mIconButton extends StatelessWidget {
-
-
+  String title;
+  var icon;
+  Color color;
+  mIconButton({required this.title,required this.icon,required this.color});
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width*0.8,
-      padding: EdgeInsets.symmetric(vertical: 20),
+      padding: EdgeInsets.symmetric(vertical: 15),
       decoration: BoxDecoration(
-          color: Config.mainColor,
+          color: color,
           borderRadius: BorderRadius.circular(30)
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Icon(Icons.facebook),
-           Text("Login",style: TextStyle(color: Colors.white),),
+          Icon(icon,color: Colors.white,),
+           Text(title,style: TextStyle(color: Colors.white,letterSpacing: 2),),
         ],
       ),
     );
